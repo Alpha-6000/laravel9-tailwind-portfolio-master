147e79f6a80c764066d269d9d9f7ba8ccacc5f17
ThierryRakotomanana <ThierryRktm@gmail.com>
2023-05-07 10:11:37 +0300

    add logo and all section

--- resources/views/components/home/portfolio.blade.php
+++ resources/views/components/home/portfolio.blade.php
@@ -24,39 +24,6 @@ En plus, l'ONIRIX peut identifier les personnes atteints de problème de santé 
         </div>
       </div>
     </div>
-    <div class="flex flex-wrap justify-center -mx-4">
-      <div class="w-full px-4">
-        <ul class="flex flex-wrap justify-center mb-12 space-x-1">
-          <li class="mb-1">
-            <button
-              @click="selectedTab = 'all' "
-              :class="selectedTab == 'all' ? activeClasses : inactiveClasses "
-              class="inline-block py-2 md:py-3 px-5 lg:px-8 rounded-lg text-base font-semibold text-center transition "
-            >
-              Racontez nous vos reves
-            </button>
-          </li>
-          @foreach($tabs as $tab)
-          <li class="mb-1">
-            <button
-              @click="selectedTab = '{{$tab}}' "
-              :class="selectedTab === '{{$tab}}' ? activeClasses : inactiveClasses "
-              class="inline-block py-2 md:py-3 px-5 lg:px-8 rounded-lg text-base font-semibold text-center transition ">
-              {{$tab}}
-            </button>
-          </li>
-          @endforeach
-        </ul>
-      </div>
-    </div>
-    <div class="flex flex-wrap -mx-4">
-      @foreach ($items as $item)
-        <x-portfolio-item :title="$item['title']"
-                          :categories="$item['category']"
-                          :image="$item['image']"
-                          :github="$item['github']"></x-portfolio-item>
-      @endforeach
-    </div>
   </div>
 </section>
 <!-- ====== Portfolio Section End -->
